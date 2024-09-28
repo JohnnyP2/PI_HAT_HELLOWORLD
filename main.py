48b3e87449059094a1104634ffa410c495f8173c
Johnny <pediconejohnny@gmail.com>
2024-09-28 18:23:23 -0400

    changed pushed_down function again

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ picture = pi_art.artfiles() # get the pixels from the array in pi_art.py
 sense.set_pixels(picture) # sets the sense hat to the pixels
 
 def pushed_down(event):
-    sense.clear()
+    sense.clear(0, 0, 0)
+    print("Ending Program")
     exit()
 
 sense.stick.direction_down = pushed_down
