88c8ed92da8cf0474ca1accd00705e0fe4b92b96
Johnny <pediconejohnny@gmail.com>
2024-09-28 16:50:11 -0400

    added to show pi_art

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import pi_art
 from sense_hat import SenseHat
 sense = SenseHat()
-
+'''
 while True:
     sense.show_message("Hello world", text_colour=(0,255,0), back_colour=(0,0,255))
+'''
+sense.set_pixels(pi_art.jack_frost)
---
+++ pi_art.py
@@ -0,0 +1,21 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+def art_files():
+    b = (0, 0, 0) # black
+    r = (255, 0, 0) # red
+    g = (0, 255, 0) # green
+    bl = (0, 0, 255) # blue
+    w = (255,255,255) # white
+
+    jack_frost = {
+        bl, w, w, w, w, w, w, bl,
+        bl, w, b, w, w, b, w, bl,
+        bl, w, b, w, w, b, w, bl,
+        w, w, w, w, w, w, w, w,
+        w, b, w, b, b, w, b, w,
+        w, b, b, b, b, b, b, w,
+        w, w, b, b, b, b, w, w,
+        w, w, w, w, w, w, w, w
+    }
+    return
