78c1b3e8ee2707e5561c71d9b8498417827ccc98
Johnny <pediconejohnny@gmail.com>
2024-09-28 17:50:19 -0400

    added comments working on joysticks

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ sense = SenseHat()
 while True:
     sense.show_message("Hello world", text_colour=(0,255,0), back_colour=(0,0,255))
 '''
-picture = pi_art.artfiles()
-sense.set_pixels(picture)
+picture = pi_art.artfiles() # get the pixels from the array in pi_art.py
+sense.set_pixels(picture) # sets the sense hat to the pixels
+
+def pushed_down(event):
+    if event.action != ACTION_RELEASED:
+        sense.clear()
+        exit()
+
+sense.stick.direction_down = pushed_down
+
