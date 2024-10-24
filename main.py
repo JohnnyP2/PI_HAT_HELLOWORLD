195783059360706422f5b6efc873fa8c97716c62
John Pedicone <56208076+JohnnyP2@users.noreply.github.com>
2024-10-24 15:29:13 +0000

    added code to use the enviornment sensors

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ while True:
 picture = pi_art.artfiles() # get the pixels from the array in pi_art.py
 sense.set_pixels(picture) # sets the sense hat to the pixels
 
+#Uses the enviornment sensors on the sense hat to show the temperature, humidity, and pressure. then Displays the data on the LED matrix.
+def enviSensor():
+    sense.clear(0,0,0)
+    temp = sense.get_temperature();
+    humidity = sense.get_humidity();
+    pressure = sense.get_pressure();
+    print("Temperature: %s C" % temp)
+    print("Humidity: %s %%" % humidity)
+    print("Pressure: %s hPa" % pressure)
+
+    
+    
+
+
+
 def pushed_down(event):
     sense.clear(0, 0, 0)
     print("Ending Program")
